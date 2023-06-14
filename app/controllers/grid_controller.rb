3be7b5142cfbfeb28f3dc1df5835a539fa41e421
Tomas Nerad <tnerad@itoperations.cz>
2023-06-14 08:10:54 +0200

    Added a from to collect input from user. Added a model "attendance.rb", updated "routes.rb" with POST method "update_attendance" Updated table in view to display contents from database.

--- app/controllers/grid_controller.rb
+++ app/controllers/grid_controller.rb
@@ -1,4 +1,16 @@
 class GridController < ApplicationController
   def attendance
+
+  end
+
+  def update_attendance
+    @records = Attendance.all
+
+    @attendance = Attendance.new()
+    if @attendance.save
+      redirect_to grid_attendance_path, notice: 'Success!'
+    else
+      render :attendance, alert: 'Failure'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  root "grid#attendance"
   get 'grid/attendance'
+  post 'grid/update_attendance'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
