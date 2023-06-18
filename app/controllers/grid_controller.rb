8637c43289dafbe6c81ca9180edfcbd2d1b33a10
Tomas Nerad <tnerad@itoperations.cz>
2023-06-18 21:17:43 +0200

    Merged 2 variables to 1 in the view and in the model. "atendances" & "records" merged to only "records".

--- app/controllers/grid_controller.rb
+++ app/controllers/grid_controller.rb
@@ -1,16 +1,19 @@
 class GridController < ApplicationController
   def attendance
-
+    @records = Attendance.all   #originaly @attendance
   end
 
   def update_attendance
-    @records = Attendance.all
-
-    @attendance = Attendance.new()
-    if @attendance.save
+    @records = Attendance.new(attendance_params)
+    @records.save
+      #@records = Attendance.all
       redirect_to grid_attendance_path, notice: 'Success!'
-    else
-      render :attendance, alert: 'Failure'
-    end
+
+    #render :attendance, alert: 'Failure'
+  end
+  private
+
+  def attendance_params
+    params.require(:attendance).permit(:student, :time_attended, :date)
   end
 end
